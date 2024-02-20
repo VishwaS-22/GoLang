b664bf15877f587feb5b4326b796fa33f2f5d91c
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2024-02-20 07:44:06 +0530

    Print set of Int

--- Tes/main.go
+++ Tes/main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	"golang.org/x/tools/go/analysis/passes/sortslice"
+    "fmt"
 )
 
 func main() {
-
-	//var list = []int{}
-	//fmt.Println("list of elements in slice", append(list, 23, 54, 36, 20, 14, 100, 1))
-	var list = []int{23, 54, 36, 20, 14, 100, 1}
-	list1 = sort.list()
-	
-	for index, v := range list{
-	
-	}
+    ets := []int{1, 2, 3, 4, 5}
+    fmt.Println("Set of integers:", ets)
 }
